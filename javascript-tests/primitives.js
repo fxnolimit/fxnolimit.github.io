47c7510110d2641b31f949e2d6434c0ca4330119
fxnolimit <fxkikomina@gmail.com>
2019-10-28 12:19:07 -0700

    js

---
+++ javascript-tests/primitives.js
@@ -0,0 +1,22 @@
+const myString = "Learning Javascript in class";
+
+var oldStyle = "used to be for changeable variable";
+let  newChangeableStyle= ' "a variable that can be changed" ';
+const nonChangeableStyle = "fixed value, can't be modified"
+
+var numeric = 123;
+var myBoolean = true;
+var myNull = null;
+var myUndefined ;
+
+console.log(myString + "\n" + myString);
+console.log(typeof myString + "\n");
+
+console.log(numeric);
+console.log('Type:',typeof numeric + "\n");
+
+console.log(myNull);
+console.log('Type:',typeof myNull, "\n");
+
+console.log(myUndefined);
+console.log('Type:',typeof myUndefined);
