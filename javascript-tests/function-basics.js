ecf3dbf4a42c49387c60f45b1770ba8070fd6fee
fxnolimit <fxkikomina@gmail.com>
2019-11-13 12:19:17 -0800

    function-basics

---
+++ javascript-tests/function-basics.js
@@ -0,0 +1,29 @@
+window.onload = () => {
+    const simpleFunctionDisplay = document.getElementById('simpleFunctionDisplay');
+    const functionCallsAction = document.getElementById('functionCallsAction');
+    functionCallsAction.onclick = simpleFunction;
+
+
+    function simpleFunction() {
+        console.log("simple function here");
+        simpleFunctionDisplay.textContent = "some text"
+        
+    }
+
+
+
+    function functionParameters(value) {
+        console.log(value);
+    }
+
+    function functionReturn(result) {
+        return "the add function returned " + result;
+    }
+
+    function add(a, b) {
+        return a + b;
+    }
+
+//simpleFunction();
+functionParameters("print me");                                                               
+}
