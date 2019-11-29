6ca9f931eb9f0eb128a330a3ce5fb5ef1543d228
fxnolimit <fxkikomina@gmail.com>
2019-11-29 13:40:11 -0800

    arrays

--- javascript-tests/javascript-arrays.js
+++ javascript-tests/javascript-arrays.js
@@ -10,17 +10,42 @@ window.onload = () => {
     const numbersAction = document.getElementById("numbers-action");
     const numbersDisplay = document.getElementById("numbers-display");
 
+    const languageAction = document.getElementById("language-action");
+    const languageDisplay = document.getElementById("language-display");
+
     const arrays = {
         numbers: [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5],
+        languages: ["JavaScript", "HTML", "CSS", "Java", "C#", "Python", "C/C++"]
     };
 
-    arrays.numbers.sort((a, b) => a - b);
 
     numbersAction.onclick = () => {
+        // before sorting
         for (let number of arrays.numbers) {
             console.log(number);
-            
+
             elfCode.appendToList(numbersDisplay, number);
+        }
+
+        arrays.numbers.sort((a, b) => a - b);
+
+        // after sorting
+        for (let number of arrays.numbers) {
+            console.log(number);
+
+            elfCode.appendToList(numbersDisplay, number);
+        }
+    }
+
+    languageAction.onclick = () => {
+        // before sorting
+        arrays.languages.sort();
+
+        // after sorting
+        for (let language of arrays.languages) {
+            console.log(language);
+
+            elfCode.appendToList(languageDisplay, language);
         }
     }
 
